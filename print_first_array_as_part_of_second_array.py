4010351e5cbda4fcc213551cc750b5c95efb4a30
Gokulavasan1012 <119430247+Gokulavasan1012@users.noreply.github.com>
2024-03-25 08:19:40 +0530

    Create print_first_array_as_part_of_second_array.py

---
+++ print_first_array_as_part_of_second_array.py
@@ -0,0 +1,13 @@
+a,b=map(int,input().split());
+l=list(map(int,input().split()));
+l1=list(map(int,input().split()))
+t=[];s=[];
+for i in l:
+    if i not in l1:
+        t.append(i);
+for i in l1:
+    if i in l:
+        for j in range(l.count(i)):
+            print(i,end=" ");
+t=sorted(t,reverse=True);
+print(*t,sep=" ",end=" ");
