7d0b1ff58770d985d609980f65ae6b04f905fa61
Gokulavasan1012 <119430247+Gokulavasan1012@users.noreply.github.com>
2024-05-17 08:51:31 +0530

    Create Expiry_Date_After_N_Months.py

---
+++ Expiry_Date_After_N_Months.py
@@ -0,0 +1,14 @@
+mon,y=map(int,input().split('-'));
+m=int(input());
+year=m//12;
+month=m-year*12;
+if (mon+month)>12:
+    year=year+(mon+month)//12;
+    mon=(mon+month)%12;
+else:
+    mon=mon+month;
+y=y+year;
+if mon<10:
+    print(f"0{mon}-{y}");
+else:
+    print(f"0{mon}-{y}");
